0d5c1021a34092aabbf7871e3aa3c09896c28d2f
strawberrypie <kjdr@kth.se>
2014-12-12 19:26:28 +0100

    Initial Commit

---
+++ ChatClientSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChatClientSwift
+//
+//  Created by Kj Drougge on 2014-12-12.
+//  Copyright (c) 2014 kj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
